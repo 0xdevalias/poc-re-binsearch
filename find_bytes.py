546253c1b545f752565d64ccb932f8fb7412256f
Glenn 'devalias' Grant <glenn@devalias.net>
2023-12-23 19:16:55 +1100

    add basic find_bytes.py script

---
+++ find_bytes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python find_bytes.py path/to/binary 'byte_sequence'")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+    byte_sequence = bytes.fromhex(sys.argv[2].replace('\\x', ''))
+
+    with open(file_path, 'rb') as file:
+        data = file.read()
+
+    idx = 0
+    while idx < len(data):
+        idx = data.find(byte_sequence, idx)
+        if idx == -1:
+            break
+        print(f'0x{idx:02x}')
+        idx += len(byte_sequence)
+
+if __name__ == "__main__":
+    main()
